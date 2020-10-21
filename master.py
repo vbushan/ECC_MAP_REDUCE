78a7be45c0d79e67e93734f6cb44d78c45d28197
Vamsi <vbushan@iu.edu>
2020-10-21 17:02:17 -0400

    Changes in master

--- master.py
+++ master.py
@@ -1,10 +1,18 @@
 import rpyc
 from rpyc.utils.server import ThreadedServer
+import configparser
 import datetime
 import socket
 import gcp_interface as cmp_eng
 import traceback
 from multiprocessing import Process
+from worker_trigger import start_worker_instance
+import os
+import concurrent.futures
+import random
+
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 
 class Master(rpyc.Service):
@@ -26,30 +34,32 @@ class Master(rpyc.Service):
 
     def exposed_init_cluster(self, num_mappers, num_reducers):
 
-        # Create Total Workers parallely
-        """
-        spawn_processes = []
-        for i in range(num_mappers):
-            spawn_processes.append(
-                Process(target=self.spawn_worker, args=('MAPPER', i+1)))
+        self.num_mappers = int(config['MASTER']['NUM_MAPPERS'])
+        self.num_reducers = int(config['MASTER']['NUM_REDUCERS'])
 
-        for i in range(num_reducers):
-            spawn_processes.append(
-                Process(target=self.spawn_worker, args=('REDUCER', i+1)))
+        # Create All Workers parallelly
 
-        """
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            mapper_names = [config['MAPPER']['NAME'] +
+                            str(i) for i in range(1, self.num_mappers+1)]
 
-        # Store mappers and reducers in a dictionary
+            mappers = executor.map(self.spawn_worker, mapper_names)
+
+            reducer_names = [config['REDUCER']['NAME'] +
+                             str(i) for i in range(1, self.num_reducers+1)]
+
+            reducers = executor.map(self.spawn_worker, reducer_names)
+
+        self.cluster['ID'] = random.randint(1, 1000)
+        self.cluster['MAPPERS'] = list(mappers)
+        self.cluster['REDUCERS'] = list(reducers)
 
         # Start KV Server
 
-        # Generate a Cluster ID
-
-        pass
-
-    def spawn_worker(self, role, index):
+    def spawn_worker(self, worker_name):
         try:
-            pass
+
+            return (worker_name, start_worker_instance(worker_name))
 
         except Exception as e:
             traceback.print_exc()
@@ -57,12 +67,22 @@ class Master(rpyc.Service):
     def spawn_kv(self):
         pass
 
-    def exposed_run_mapred(self, a, b):
+    def exposed_run_mapred(self, in_loc, map_func, red_func, out_loc):
+
+        input_chunks = [[] for i in range(self.num_mappers)]
 
         # Read Input
+        if os.path.isdir(in_loc):
+            index = 0
+            for file in os.listdir(in_loc):
+                input_chunks[index] = open(os.path.join(
+                    './books', file), 'r', encoding='utf-8').read()
+                index += 1
+
         # Store Input in KV
 
         # Start Mapper execute
+
         # Stop Mappers
 
         # Start Reducer execute
@@ -78,7 +98,8 @@ class Master(rpyc.Service):
 
 if __name__ == "__main__":
 
-    t = ThreadedServer(Master, hostname=socket.gethostbyname(
-        socket.gethostname()), port=8080)
+    # t = ThreadedServer(Master, hostname=socket.gethostbyname(
+    #    socket.gethostname()), port=8080)
 
+    t = ThreadServer(Master, hostname='0.0.0.0', port=8080)
     t.start()
--- master_trigger.py
+++ master_trigger.py
@@ -3,6 +3,8 @@ import configparser
 import gcp_interface as cmp_eng
 import pprint
 import traceback
+from multiprocessing import Process
+import worker_functions
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -12,24 +14,39 @@ startup_script = open(
     os.path.join(
         os.path.dirname(__file__), 'master-startup-script.sh'), 'r').read()
 
-compute = googleapiclient.discovery.build('compute', 'v1')
 
-
-project = config['MAP_REDUCE']['PROJECT_ID']
-zone = config['MAP_REDUCE']['ZONE']
+PROJECT = config['MAP_REDUCE']['PROJECT_ID']
+ZONE = config['MAP_REDUCE']['ZONE']
 NAME = config['MASTER']['NAME']
+NUM_MAPPERS = config['MASTER']['NUM_MAPPERS']
+NUM_REDUCERS = config['MASTER']['NUM_REDUCERS']
+# INPUT_LOCATION =
 
 try:
 
-    create_op = create_instance(compute, project, zone,
-                                'sample-master', startup_script)
+    create_op = cmp_eng.create_instance(PROJECT, ZONE,
+                                        NAME, startup_script)
 
     print('Creating Master instance....')
 
-    status = wait_for_operation(compute, project, zone, create_op['name'])
+    status = wait_for_operation(project, zone, create_op['name'])
     pprint(status)
 
     print('[Checkpoint] Master Instance Created')
+
+    MASTER_IP = cmp_eng.get_ip(PROJECT, ZONE, NAME)
+    MASTER_PORT = int(config['MAP_REDUCE']['PORT'])
+
+    print('MASTER NODE ADDRESS', (MASTER_IP, MASTER_PORT))
+
+    #conn = rpyc.connect(MASTER_IP, MASTER_PORT)
+
+    #master_node = conn.root
+
+    #master_node.init_cluster(NUM_MAPPERS, NUM_REDUCERS)
+
+    # master_node.map_reduce()
+
 
 except Exception as e:
     traceback.print_exc()
